123e07d0949dc41f432fbe6db83aefa2fd1ec07a
Arya Jayadi <arya.jayadi2013@gmail.com>
2022-07-04 07:09:25 +0700

    burger

--- AXForPatty/src/models/Burger.java
+++ AXForPatty/src/models/Burger.java
@@ -4,8 +4,44 @@ public class Burger {
 	private int patty;
 	private int cheese;
 	private int bun;
-	private boolean lettuce;
-	private boolean tomato;
-	private boolean onion;
+	private int price;
+	private boolean vegetable;
 	private boolean pickle;
+	private boolean smash;
+	private String name;
+	
+	public Burger() {
+		super();
+		this.patty = 1;
+		this.cheese = 1;
+		this.bun = 2;
+		this.price = (patty * 10000) + (cheese * 5000) + (bun * 2000);
+		this.vegetable = true;
+		this.pickle = true;
+		this.smash = false;
+		this.name = "AXBurPatt";
+	}
+
+	public void viewBurger() {
+		System.out.printf("%10s: %s\n", "Name", name);
+		System.out.printf("%10s: %d\n", "Bun", bun);
+		System.out.printf("%10s: %d\n", "Pattty", patty);
+		System.out.printf("%10s: %d\n", "Cheese", cheese);
+		System.out.printf("%10s: %s\n", "Vegetable", vegetable ? "included" : "not included");
+		System.out.printf("%10s: %s\n", "Pickle", pickle ? "included" : "not included");
+		System.out.printf("%10s: %s\n", "Smash", smash ? "smash burger" : "normal burger");
+		System.out.printf("%10s: IDR %d\n", "Price", price);
+	}
+	
+	public void purchaseBurger(int money) {
+		if(money > price) {
+			System.out.println("Thankyou for your purchase!");
+			System.out.printf("Your return is: IDR %d\n", money - price);
+		} else if(money < price) {
+			System.out.println("Sorry, that wasn't enough.");
+			System.out.printf("You lack: IDR %d\n", price - money);
+		} else {
+			System.out.println("Thankyou for your purchase!");
+		}
+	}
 }
