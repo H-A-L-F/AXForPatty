21762a13d128cca9714a0fa9d41d2da3d13500ab
Arya Jayadi <arya.jayadi2013@gmail.com>
2022-07-04 08:19:04 +0700

    adapter

---
+++ AXForPatty/src/modules/RupiahAdapter.java
@@ -0,0 +1,13 @@
+package modules;
+
+public class RupiahAdapter {
+	private String name;
+	private double multiplier;
+	
+	public RupiahAdapter(String name) {
+		super();
+		this.name = name;
+		this.multiplier = multiplier;
+	}
+	
+}
